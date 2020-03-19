141e6b3e2e2907895dd0575554f7bbc6acb9d456
Jakub Kondrat <jkondrat@soldevelo.com>
2020-03-19 16:22:34 +0100

    Initial version of ServiceNetAuth generated by JHipster-6.7.1

---
+++ src/main/java/org/benetech/servicenet/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package org.benetech.servicenet.repository;
+
+import org.benetech.servicenet.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
