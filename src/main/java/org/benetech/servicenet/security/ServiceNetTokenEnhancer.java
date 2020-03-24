cfe431eac072264be45df5aa67e4295e64d2dadf
Jakub Kondrat <jkondrat@soldevelo.com>
2020-03-24 19:09:54 +0100

    #877: Enahnce the tokens with 'user_id'

--- src/main/java/org/benetech/servicenet/security/ServiceNetTokenEnhancer.java
+++ src/main/java/org/benetech/servicenet/security/ServiceNetTokenEnhancer.java
@@ -10,27 +10,30 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
+ * Adds "user_id" to the token so we can identify users in other microservices.
  * Adds the standard "iat" claim to tokens so we know when they have been created.
  * This is needed for a session timeout due to inactivity (ignored in case of "remember-me").
  */
 @Component
-public class IatTokenEnhancer implements TokenEnhancer {
+public class ServiceNetTokenEnhancer implements TokenEnhancer {
 
     @Override
     public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
-        addClaims((DefaultOAuth2AccessToken) accessToken);
+        addClaims((DefaultOAuth2AccessToken) accessToken, authentication.getPrincipal());
         return accessToken;
     }
 
-    private void addClaims(DefaultOAuth2AccessToken accessToken) {
-        DefaultOAuth2AccessToken token = accessToken;
-        Map<String, Object> additionalInformation = token.getAdditionalInformation();
+    private void addClaims(DefaultOAuth2AccessToken accessToken, Object principal) {
+        Map<String, Object> additionalInformation = accessToken.getAdditionalInformation();
         if (additionalInformation.isEmpty()) {
             additionalInformation = new LinkedHashMap<String, Object>();
+        }
+        if (principal instanceof CustomSpringSecurityUser) {
+            additionalInformation.put("user_id", ((CustomSpringSecurityUser) principal).getId());
         }
         //add "iat" claim with current time in secs
         //this is used for an inactive session timeout
         additionalInformation.put("iat", new Integer((int)(System.currentTimeMillis()/1000L)));
-        token.setAdditionalInformation(additionalInformation);
+        accessToken.setAdditionalInformation(additionalInformation);
     }
 }
