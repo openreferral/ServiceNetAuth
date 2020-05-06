a7050442a7276bb4d624da22285eff9f99277b39
kdondziak <52406204+kdondziak@users.noreply.github.com>
2020-05-06 17:22:45 +0200

    benetech/ServiceNet#783: Add unique id message (#11)

--- src/main/java/org/benetech/servicenet/web/rest/errors/IdAlreadyUsedException.java
+++ src/main/java/org/benetech/servicenet/web/rest/errors/IdAlreadyUsedException.java
@@ -1,10 +1,13 @@
 package org.benetech.servicenet.web.rest.errors;
 
+import org.zalando.problem.Status;
+
 public class IdAlreadyUsedException extends BadRequestAlertException {
 
     private static final long serialVersionUID = 1L;
 
     public IdAlreadyUsedException() {
-        super(ErrorConstants.ID_ALREADY_USED_TYPE, "Id already used!", "clientManagement", "clientexists");
+        super(ErrorConstants.ID_ALREADY_USED_TYPE, "Id already used!", "clientManagement", "clientexists",
+            Status.CONFLICT);
     }
 }
